f7936c9821ff04bea4e9d293c051838f35f6b0bb
faustienf <you@example.com>
2022-08-01 18:30:14 +0300

    update render

--- src/request-stripe.ts
+++ src/request-stripe.ts
@@ -1,59 +1,50 @@
 import styles from './request-stripe.css';
 
-export type Render = (onComplete: (error: Error | null) => void) => () => void;
+export type Render = (onComplete: () => void) => () => void;
 
 const random = () => Math.random().toString(32).slice(2);
 const generateToken = () => random() + random();
 const requests = new Set<string>();
+const processes = new Set<ReturnType<Render>>();
 
 export const defaultRender: Render = (onComplete) => {
   const stripeElement = document.createElement('div');
-  stripeElement.classList.add('RequestStripe');
   stripeElement.classList.add(styles['request-stripe']);
+  stripeElement.classList.add('request-stripe-custom');
   stripeElement.dataset.state = 'process';
 
   stripeElement.addEventListener('animationend', () => {
+    onComplete();
+
     document.body.removeChild(stripeElement);
 
-    const error =
-      stripeElement.dataset.state !== 'finish'
-        ? new Error("Request hasn't been finished correctly")
-        : null;
-
-    onComplete(error);
+    if (stripeElement.dataset.state !== 'finish') {
+      throw new Error("Request hasn't been finished correctly");
+    }
   });
 
   document.body.appendChild(stripeElement);
 
   return () => {
+    onComplete();
     // finish animation and trigger animationend Event
     stripeElement.dataset.state = 'finish';
   };
 };
 
-const startProcess = (() => {
-  let endProcess: null | (() => void) = null;
-
-  return (render: Render) => {
-    if (endProcess) {
-      return endProcess;
-    }
-
-    const finish = render((error) => {
-      if (error) {
-        endProcess = null;
-        throw error;
-      }
-    });
-
-    endProcess = () => {
-      finish();
-      endProcess = null;
-    };
-
+const startProcess = (render: Render) => {
+  const endProcess = [...processes].at(-1);
+  if (endProcess) {
     return endProcess;
-  };
-})();
+  }
+
+  const finish = render(() => {
+    processes.delete(finish);
+  });
+  processes.add(finish);
+
+  return finish;
+};
 
 type Options = {
   render: Render;
