a9756f50eabca26bfdfc50603887c24c2adac092
faustienf <you@example.com>
2022-08-05 13:23:05 +0300

    fix header

--- example/src/main.ts
+++ example/src/main.ts
@@ -2,7 +2,7 @@ import './style.css';
 import { requestStripe } from 'request-stripe';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <button id="run" type="button">requestStrips()</button>
+  <button id="run" type="button">requestStripe()</button>
 `;
 
 document
